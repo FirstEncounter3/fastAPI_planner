324a6f9099c3e0ad92fff70404f07fec8f7137ca
First_Encounter <needenergy27@gmail.com>
2024-09-20 01:58:42 +0700

    adapted to work with mongoDB

--- routes/events.py
+++ routes/events.py
@@ -1,49 +1,68 @@
-from fastapi import APIRouter, Body, HTTPException, status
-from models.events import Event
+from beanie import PydanticObjectId
+from fastapi import APIRouter, HTTPException, status
+from database.connection import Database
+
+
+from models.events import Event, EventUpdate
+
+event_database = Database(Event)
 
 
 event_router = APIRouter(
     tags=["Events"],
 )
 
-events = []
-
 
 @event_router.get("/", response_model=list[Event])
 async def retrieve_all_events() -> list[Event]:
+    events = await event_database.get_all()
     return events
 
 
 @event_router.get("/{id}", response_model=Event)
-async def retrieve_event(id: int) -> Event:
-    for event in events:
-        if event.id == id:
-            return event
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="Event not found",
-    )
+async def retrieve_event(id: PydanticObjectId) -> Event:
+    event = await event_database.get(id)
+    if not event:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Event not found",
+        )
+
+    return event
 
 
 @event_router.post("/new")
-async def create_event(body: Event = Body(...)) -> dict:
-    events.append(body)
+async def create_event(body: Event) -> dict:
+    await event_database.save(body)
     return {"message": "Event created successfully"}
 
 
+@event_router.put("/{id}", response_model=Event)
+async def update_event(id: PydanticObjectId, body: EventUpdate) -> Event:
+    updateed_event = await event_database.update(id, body)
+    if not updateed_event:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Event not found",
+        )
+
+    return updateed_event
+
+
 @event_router.delete("/{id}")
-async def delete_event(id: int) -> dict:
-    for event in events:
-        if event.id == id:
-            events.remove(event)
-            return {"message": "Event deleted successfully"}
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="Event not found",
-    )
+async def delete_event(id: PydanticObjectId) -> dict:
+    deleted_event = await event_database.delete(id)
+    if not deleted_event:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Event not found",
+        )
+
+    return {"message": "Event deleted successfully"}
 
 
+# experimental
 @event_router.delete("/")
 async def delete_all_events() -> dict:
-    events.clear()
+    await event_database.delete_all()
     return {"message": "All events deleted successfully"}
--- routes/users.py
+++ routes/users.py
@@ -1,32 +1,36 @@
 from fastapi import APIRouter, HTTPException, status
+from database.connection import Database
+
 from models.users import User, UserSignIn
 
 user_router = APIRouter(
     tags=["User"],
 )
 
-users = {}
+user_database = Database(User)
 
 
 @user_router.post("/signup")
-async def sign_new_user(data: User) -> dict:
-    if data.email in users:
+async def sign_new_user(user: User) -> dict:
+    user_exist = await User.find_one(User.email == user.email)
+    if user_exist:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail="User with this email already exists",
         )
-    users[data.email] = data
+    await user_database.save(user)
     return {"message": "User created successfully"}
 
 
 @user_router.post("/signin")
 async def sign_user_in(user: UserSignIn) -> dict:
-    if user.email not in users:
+    user_exist = await User.find_one(User.email == user.email)
+    if not user_exist:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="User not found",
         )
-    if users[user.email].password != user.password:
+    if user_exist.password != user.password:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Invalid credentials",
@@ -35,3 +39,11 @@ async def sign_user_in(user: UserSignIn) -> dict:
     return {
         "message": "User logged in successfully",
     }
+
+
+# experimental
+
+@user_router.get("/users")
+async def get_all_users() -> list[User]:
+    users = await user_database.get_all()
+    return users

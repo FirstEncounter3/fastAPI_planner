a0e45f9a1c0571b92e8dedc6fb5a400787a0054e
First_Encounter <needenergy27@gmail.com>
2024-09-20 01:56:05 +0700

    refactoring for pydantic v2.0+ and update for work with mongoDVB

--- models/events.py
+++ models/events.py
@@ -1,17 +1,18 @@
-from pydantic import BaseModel
+from beanie import Document
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Optional
 
 
-class Event(BaseModel):
-    id: int
+class Event(Document):
     title: str
     image: str
     description: str
     tags: list[str]
     location: str
 
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": [
                 {
                     "title": "FastAPI book launch",
                     "image": "https://linktomyimg.com/img.png",
@@ -21,4 +22,27 @@ class Event(BaseModel):
                 }
             ]
         }
-    }
+    )
+
+    class Settings:
+        name = "events"
+
+
+class EventUpdate(BaseModel):
+    title: Optional[str] = Field(default=None)
+    image: Optional[str] = Field(default=None)
+    description: Optional[str] = Field(default=None)
+    tags: Optional[list[str]] = Field(default=None)
+    location: Optional[str] = Field(default=None)
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet",
+            }
+        }
+    )

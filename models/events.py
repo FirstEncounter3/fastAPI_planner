adf6332d9788bf82f3cefda0277aaac067ae3888
First_Encounter <needenergy27@gmail.com>
2024-09-19 23:21:16 +0700

    update models to action with db

--- models/events.py
+++ models/events.py
@@ -1,24 +1,45 @@
-from pydantic import BaseModel
+from sqlmodel import JSON, SQLModel, Field, Column
+from pydantic import ConfigDict
+from typing import Optional
 
 
-class Event(BaseModel):
-    id: int
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: list[str]
+    tags: list[str] = Field(sa_column=Column(JSON))
     location: str
 
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "title": "FastAPI book launch",
-                    "image": "https://linktomyimg.com/img.png",
-                    "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
-                    "tags": ["python", "fastapi", "book", "launch"],
-                    "location": "San Francisco, CA",
-                }
-            ]
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        json_schema_extra={
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet",
+            }
+        },
+    )
+
+
+class EventUpdate(SQLModel):
+    title: Optional[str] = Field(default=None)
+    image: Optional[str] = Field(default=None)
+    desctiption: Optional[str] = Field(default=None)
+    tags: Optional[list[str]] = Field(default=None)
+    location: Optional[str] = Field(default=None)
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet",
+            }
         }
-    }
+    )

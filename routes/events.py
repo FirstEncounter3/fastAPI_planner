b5fbef694ff94c02a9f7c964f8948c4a824bc365
First_Encounter <needenergy27@gmail.com>
2024-09-25 18:24:18 +0700

    add creator when creating an entity, check for creator: only creator can delete or update the entity

--- routes/events.py
+++ routes/events.py
@@ -35,25 +35,39 @@ async def retrieve_event(id: PydanticObjectId) -> Event:
 
 @event_router.post("/new")
 async def create_event(body: Event, user: str = Depends(authenticate)) -> dict:
+    body.creator = user['user']
     await event_database.save(body)
     return {"message": "Event created successfully"}
 
 
 @event_router.put("/{id}", response_model=Event)
 async def update_event(id: PydanticObjectId, body: EventUpdate, user: str = Depends(authenticate)) -> Event:
-    updateed_event = await event_database.update(id, body)
-    if not updateed_event:
+    updated_event = await event_database.update(id, body)
+
+    if updated_event.creator != user['user']:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Operation not allowed",
+        )
+    if not updated_event:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Event not found",
         )
 
-    return updateed_event
+    return updated_event
 
 
 @event_router.delete("/{id}")
 async def delete_event(id: PydanticObjectId, user: str = Depends(authenticate)) -> dict:
     deleted_event = await event_database.delete(id)
+
+    if deleted_event.creator != user["user"]:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Operation not allowed",
+        )
+
     if not deleted_event:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,

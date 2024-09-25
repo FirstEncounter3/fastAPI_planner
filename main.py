83acadc2d545a1930168788e593a5aa813a19d8a
First_Encounter <needenergy27@gmail.com>
2024-09-25 21:57:07 +0700

    add cors middleware

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.users import user_router
 from routes.events import event_router
@@ -11,16 +12,26 @@ from database.connection import Settings
 import uvicorn
 
 settings = Settings()
+origins = ["*"]
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await settings.initialize_database()
     yield
 
+
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")

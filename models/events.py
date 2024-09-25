76c1333bf561299ddd9d42ec7aae2b9ce4604056
First_Encounter <needenergy27@gmail.com>
2024-09-25 10:42:36 +0700

    fix example schemas

--- models/events.py
+++ models/events.py
@@ -12,15 +12,13 @@ class Event(Document):
 
     model_config = ConfigDict(
         json_schema_extra={
-            "example": [
-                {
-                    "title": "FastAPI book launch",
-                    "image": "https://linktomyimg.com/img.png",
-                    "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
-                    "tags": ["python", "fastapi", "book", "launch"],
-                    "location": "San Francisco, CA",
-                }
-            ]
+            "example": {
+                "title": "FastAPI book launch",
+                "image": "https://linktomyimg.com/img.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "San Francisco, CA",
+            }
         }
     )
 

acc61648686decc4b2e74134ffd0dac1d7c64c66
First_Encounter <needenergy27@gmail.com>
2024-09-27 13:36:42 +0700

    refactoring, cleanup_db in separate fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,18 +8,25 @@ from database.connection import Settings
 from models.events import Event
 from models.users import User
 
+
 async def init_test_db():
     test_settings = Settings()
     test_settings.DATABASE_URL = "mongodb://localhost:27017/test"
 
     await test_settings.initialize_database()
 
+
 @pytest.fixture
 async def default_client():
     await init_test_db()
-    async with httpx.AsyncClient(transport=ASGITransport(app=app), base_url="http://app") as client:
+    async with httpx.AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://app"
+    ) as client:
         yield client
-        # cleanup
-        await Event.find_all().delete()
-        await User.find_all().delete()
-    
+
+
+@pytest.fixture
+async def cleanup_db():
+    yield
+    await Event.find_all().delete()
+    await User.find_all().delete()
